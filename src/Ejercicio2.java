83523a6a9dc4cf02e5af6d3f96df2ef4656b25c5
Elias Barreto <eliasgobarreto@gmail.com>
2023-03-31 22:19:12 -0300

    Segundo Commit

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,12 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+import java.util.Scanner;
+
 
 /**
- *
+ Diseñe una función que pida el nombre y la edad de N personas e imprima los 
+ * datos de las personas ingresadas por teclado e indique si son mayores o 
+ * menores de edad. Después de cada persona, el programa debe preguntarle al
+ * usuario si quiere seguir mostrando personas y frenar cuando el usuario 
+ * ingrese la palabra “No”.
  * @author elias
  */
 public class Ejercicio2 {
+    public static void main(String[] args){
+        String noMbre;
+        int eDad, nPer,i;
+        Scanner leerN= new Scanner(System.in);
+        System.out.println("Introduzca la cantidad de personas: ");
+        nPer=leerN.nextInt();
+        i=1;
+        
+        
+        
+        do {            
+            i++;
+            
+            
+        } while (i<=nPer);
+    }
+    
+    public static void lista(String noMbre, int eDad,int nPer){
+    
+        for (int i = 0; i < nPer; i++) {
+            
+        }
+    }
     
 }
--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -1,12 +1,70 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+import com.sun.source.tree.IfTree;
+import java.util.Scanner;
 
 /**
+ * Crea una aplicación que a través de una función nos convierta una cantidad de
+ * euros introducida por teclado a otra moneda, estas pueden ser a dólares,
+ * yenes o libras. La función tendrá como parámetros, la cantidad de euros y la
+ * moneda a convertir que será una cadena, este no devolverá ningún valor y
+ * mostrará un mensaje indicando el cambio (void). El cambio de divisas es: 0.86
+ * libras es un 1 € 1.28611 $ es un 1 € 129.852 yenes es un 1 €
  *
  * @author elias
  */
 public class Ejercicio3 {
-    
+
+    public static void main(String[] args) {
+        int euRo, moNeda;
+        double doLar, liBra, yEn;
+
+        Scanner leerE = new Scanner(System.in);
+        System.out.println("Ingrese la cantidad de Euros: ");
+        euRo = leerE.nextInt();
+
+        do {
+            
+            System.out.println("A que moneda desea convertir?");
+            System.out.println("Libras -> 1");
+            System.out.println("Dolares -> 2");
+            System.out.println("Yenes -> 3");
+            System.out.println("Finalizar -> 4");
+            moNeda = leerE.nextInt();
+            liBra = converL(moNeda, euRo);
+            doLar = converD(moNeda, euRo);
+            yEn = converY(moNeda, euRo);
+
+            switch (moNeda) {
+                case 1:
+                    System.out.println(liBra);
+                    break;
+                case 2:
+                    System.out.println(doLar);
+                    break;
+                case 3:
+                    System.out.println(yEn);
+                    break;
+            }
+        } while (moNeda != 4 && moNeda<4);
+
+    }
+
+    public static double converL(int moNeda, int euRo) {
+        double cL;
+        cL = 0.86 * euRo;
+        return cL;
+    }
+
+    public static double converD(int moNeda, int euRo) {
+        double cD;
+        cD = 1.28611 * euRo;
+        return cD;
+    }
+
+    public static double converY(int moNeda, int euRo) {
+        double cY;
+        cY = 129.852 * euRo;
+        return cY;
+    }
+
 }
